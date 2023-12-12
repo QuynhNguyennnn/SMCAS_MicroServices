5e46a0f487142c1032748cd00df0bc1c12875cab
Tam Le <tamminhle2002@gmail.com>
2023-12-12 14:35:38 +0700

    update

--- BlogService/Models/Medicine.cs
+++ BlogService/Models/Medicine.cs
@@ -25,8 +25,6 @@ public partial class Medicine
 
     public virtual ICollection<MedicineExaminatedRecord> MedicineExaminatedRecords { get; set; } = new List<MedicineExaminatedRecord>();
 
-    public virtual ICollection<MedicineExaminatedRecord> MedicineExaminatedRecords { get; set; } = new List<MedicineExaminatedRecord>();
-
     public virtual Unit User { get; set; } = null!;
 
     public virtual User UserNavigation { get; set; } = null!;
