247cce72b8779c89662c2245b1670747e315ef70
Quynhcuteeee <quynhnguyenst1610@gmail.com>
2023-12-19 15:14:29 +0700

    update getDoctors

--- MedicineService/Services/ExaminatedRecordFolder/ExaminatedRecordService.cs
+++ MedicineService/Services/ExaminatedRecordFolder/ExaminatedRecordService.cs
@@ -7,8 +7,12 @@ namespace MedicineService.Services.ExaminatedRecordFolder
     {
         public ExaminatedRecord CreateRecord(ExaminatedRecord record) => ExaminatedRecordDAO.CreateRecord(record);
 
+        public ExaminatedRecord DeleteRecord(int id) => ExaminatedRecordDAO.DeleteRecord(id);
+
         public List<ExaminatedRecord> GetAll() => ExaminatedRecordDAO.GetAll();
 
         public ExaminatedRecord GetRecordById(int id) => ExaminatedRecordDAO.GetRecordById(id);
+
+        public ExaminatedRecord UpdateRecord(ExaminatedRecord record) => ExaminatedRecordDAO.UpdateRecord(record);
     }
 }
