72e8df7fc685478f5a5cf7976ed3804f0c0db010
Tam Le <tamminhle2002@gmail.com>
2024-01-19 21:44:56 +0700

    Commit

--- UserService/DTOs/UpdateRoleRequest.cs
+++ UserService/DTOs/UpdateRoleRequest.cs
@@ -6,6 +6,20 @@
 
         public string RoleName { get; set; } = null!;
 
+        public string User { get; set; }
+
+        public string Blog { get; set; }
+
+        public string Medicine { get; set; }
+
+        public string ExaminatedRecord { get; set; }
+
+        public string Feedback { get; set; }
+
+        public string Schedule { get; set; }
+
+        public string Chat { get; set; }
+
         public bool IsActive { get; set; }
     }
 }
